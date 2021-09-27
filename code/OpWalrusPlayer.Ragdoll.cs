5c767bf3fd4e14e2d37017efb17728d4bfc479fb
Rugg <colony4ever@gmail.com>
2021-09-26 20:28:48 -0500

    compiler error fixes

--- code/OpWalrusPlayer.Ragdoll.cs
+++ code/OpWalrusPlayer.Ragdoll.cs
@@ -23,7 +23,7 @@ namespace OpWalrus
 			ent.EnableHitboxes = true;
 			ent.EnableAllCollisions = true;
 			ent.SurroundingBoundsMode = SurroundingBoundsType.Physics;
-			ent.RenderColorAndAlpha = RenderColorAndAlpha;
+			ent.RenderColor = RenderColor;
 			ent.PhysicsGroup.Velocity = velocity;
 
 			if ( Local.Pawn == this )
@@ -46,7 +46,7 @@ namespace OpWalrus
 					var clothing = new ModelEntity();
 					clothing.SetModel( model );
 					clothing.SetParent( ent, true );
-					clothing.RenderColorAndAlpha = e.RenderColorAndAlpha;
+					clothing.RenderColor = e.RenderColor;
 
 					if ( Local.Pawn == this )
 					{
